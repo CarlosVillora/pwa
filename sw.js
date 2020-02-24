2aa8310b555e0a42c7da5509191e91cab6e157b4
CarlosVillora <carlos@unicarlos.com>
2020-02-24 16:12:30 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('video-store').then(function(cache) {
+     return cache.addAll([
+       '/pwa/',
+       '/pwa/index.html',
+       '/pwa/index.js',
+       '/pwa/index.css',
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
